a86408a1e0725fdc55e92da161feb268f0b8eaf8
unknown <fethisert0@gmail.com>
2023-12-18 15:37:53 +0200

    Basic LocalStorage with React Learned

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
 import './App.css';
+import Split from 'react-split'
+import { useEffect, useState  } from 'react';
+import InputText from './components/InputText';
+import {v4 as uuidv4} from 'uuid'
 
+//Oncelikli amac statin doldurulmasi ve onun uzerinden calisilmasi ne zaman ki state degisir useEffect kullanacak state te tutulan bilgiler islenir
+//Datalari state yukle state i kullanarak uygulmada degisim yap ve state ile disayi bilgilendir
+//Her sey state kontorlunda olmali ki degisim yapildiginda re render yapabilelim 
 function App() {
+  const [input, setInput] = useState({bookTitle: '', author: ''});
+  const [books, setBook] = useState(()=>{
+    const localData = localStorage.getItem('books');
+    return localData ? JSON.parse(localData) : [];
+  });
+
+  useEffect(()=>{
+    localStorage.setItem('books',JSON.stringify(books));
+  },[books])
+
+  const bookElements = books.length > 0 ? books.map((bookEl,index) => {
+    return (
+      <li key={bookEl.id}>
+        <span className='book-title'>{ bookEl.bookTitle }</span>
+        <span className='book-author'>{ bookEl.author }</span>
+        <span className='book-number'>{ index + 1 }</span>
+      </li>);
+  }): null;
+
+  function handleInput(e){
+    const {name, value} = e.target;
+    setInput(prevState =>({...prevState,[name]:value}));
+  }
+
+  function checkInput(){
+    if(input.bookTitle !== '' && input.author !== ''){
+      return true
+    }else{
+      return false
+    }
+  }
+
+  function createBook(){
+    console.log(localStorage);
+    if(checkInput()){
+
+      const book = {
+        id:uuidv4(),
+        bookTitle: input.bookTitle,
+        author:input.author
+      }
+
+      setBook(prevState => {
+        console.log(prevState);
+        return [...prevState,book];
+      });
+
+    }else{
+      alert("Fill all boxes please");
+    }
+  }
+
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Ninja Reading List</h1>
+        <p>Currently you have <span>{books.length}</span> books to het thourgh</p>
       </header>
+
+      <main>
+        <InputText name='bookTitle' value={input.bookTitle}  onChange={ handleInput }  placeholder='book title' />
+        <InputText name='author' value={input.author}  onChange={ handleInput }  placeholder='author' />
+        <button onClick={ createBook }>Add Book</button>
+        {books.length === 0 ? <p>No books to read.</p>: <ul className='book-list'>{ bookElements }</ul>}
+      </main>
     </div>
   );
+
+
 }
 
+
+
 export default App;
+
+///#482B46 outside
+//#71436F header
+// #40233D input
+// #4E2F4E input container
---
+++ src/components/InputText.js
@@ -0,0 +1,10 @@
+import '../css/InputText.css'
+
+function InputText(props){
+    return (
+        <input  className="input-text" name={props.name} value={props.value} onChange={props.onChange} placeholder={props.placeholder} required></input>
+    );
+}
+
+
+export default InputText;
